c2e6163442a81a64802f80e4435cc850cc563efc
John Marzulli <john.marzulli@hotmail.com>
2025-09-26 23:05:08 -0700

    Update the config checker to be more Pythonic 3

--- check_config_files.py
+++ check_config_files.py
@@ -1,16 +1,12 @@
 # Self-Test file that makes sure all
 # off the station identifiers are OK.
 
-import time
-
 from configuration import configuration
 from data_sources import weather
 from lib import safe_logging
 
 
-def terminal_error(
-    error_message
-):
+def terminal_error(error_message):
     safe_logging.safe_log_warning(error_message)
     exit(0)
 
@@ -19,15 +15,16 @@ try:
     airport_render_config = configuration.get_airport_configs()
 except Exception as e:
     terminal_error(
-        'Unable to fetch the airport configuration. Please check the JSON files. Error={}'.format(e))
+        f"Unable to fetch the airport configuration. Please check the JSON files. Error={e}"
+    )
 
 if len(airport_render_config) == 0:
-    terminal_error('No airports found in the configuration file.')
+    terminal_error("No airports found in the configuration file.")
 
 stations_unable_to_fetch_weather = []
 
 for station_id in airport_render_config:
-    safe_logging.safe_log('Checking configuration for {}'.format(station_id))
+    safe_logging.safe_log(f"Checking configuration for {station_id}")
 
     led_indices = airport_render_config[station_id]
 
@@ -35,22 +32,25 @@ for station_id in airport_render_config:
     for led_index in led_indices:
         if led_index < 0:
             terminal_error(
-                'Found {} has an LED at a negative position {}'.format(
-                    station_id,
-                    led_index))
+                f"Found {station_id} has an LED at a negative position {led_index}"
+            )
 
     # Validate that the station is in the CSV file
     try:
         data_file_icao_code = weather.get_faa_csv_identifier(station_id)
     except Exception as e:
         terminal_error(
-            'Unable to fetch the station {} from the CSV data file. Please check that the station is in the CSV file. Error={}'.format(station_id, e))
+            f"Unable to fetch the station {station_id} from the CSV data file. Please check that the station is in the CSV file. Error={e}"
+        )
 
-    if data_file_icao_code is None or data_file_icao_code == '' or weather.INVALID in data_file_icao_code:
+    if (
+        data_file_icao_code is None
+        or data_file_icao_code == ""
+        or weather.INVALID in data_file_icao_code
+    ):
         terminal_error(
-            'Unable to fetch the station {} from the CSV data file. Please check that the station is in the CSV file. Error={}'.format(
-                station_id,
-                e))
+            f"Unable to fetch the station {station_id} from the CSV data file. Please check that the station is in the CSV file. Error=None"
+        )
 
     # Validate that the station can have weather fetched
     metar = weather.get_metar(station_id)
@@ -58,36 +58,32 @@ for station_id in airport_render_config:
     if metar is None or weather.INVALID in metar:
         stations_unable_to_fetch_weather.append(station_id)
         safe_logging.safe_log_warning(
-            'Unable to fetch weather for {}/{}'.format(
-                station_id,
-                led_indices))
+            f"Unable to fetch weather for {station_id}/{led_indices}"
+        )
 
     # Validate that the station can have Sunrise/Sunset fetched
     day_night_info = weather.get_civil_twilight(station_id)
 
     if day_night_info is None:
-        terminal_error(
-            'Unable to fetch day/night info for {}/{}'.format(
-                station_id,
-                led_indices))
+        terminal_error(f"Unable to fetch day/night info for {station_id}/{led_indices}")
 
     if len(day_night_info) != 6:
         terminal_error(
-            'Unknown issue fetching day/night info for {}/{}'.format(
-                station_id,
-                led_indices))
+            f"Unknown issue fetching day/night info for {station_id}/{led_indices}"
+        )
 
-safe_logging.safe_log('')
-safe_logging.safe_log('')
-safe_logging.safe_log('-------------------------')
+safe_logging.safe_log("")
+safe_logging.safe_log("")
+safe_logging.safe_log("-------------------------")
 safe_logging.safe_log(
-    'Finished testing configuration files. No fatal issues were found.')
-safe_logging.safe_log('')
-safe_logging.safe_log(
-    'Unable to fetch the weather for the following stations:')
+    "Finished testing configuration files. No fatal issues were found."
+)
+safe_logging.safe_log("")
+safe_logging.safe_log("Unable to fetch the weather for the following stations:")
 
 for station in stations_unable_to_fetch_weather:
-    safe_logging.safe_log('\t {}'.format(station))
+    safe_logging.safe_log(f"\t {station}")
 
 safe_logging.safe_log(
-    'Please check the station identifier. The station may be out of service, temporarily down, or may not exist.')
+    "Please check the station identifier. The station may be out of service, temporarily down, or may not exist."
+)

1ff49f57d41ab47f0cd8f77ded4e7d2ea71d4184
John Marzulli <john.marzulli@hotmail.com>
2025-11-01 13:03:05 -0700

    Simplify the halloween color rotation

--- visualizers/halloween_lights_visualizer.py
+++ visualizers/halloween_lights_visualizer.py
@@ -16,28 +16,31 @@ class HalloweenLightsVisualizer(Visualizer):
         # The lower the adjustment, the slower the lights. 0.5 is half the speed.
         # The higher the adjustment, the faster the lights. 2 is twice the speed.
         self.__speed_adjustment__ = 8
+        self.__incremental_index__: float = 0.0
         super().__init__(renderer, stations)
 
     def update(self, time_slice: float):
-        current_seconds = datetime.now(timezone.utc).second * self.__speed_adjustment__
+        elapsed_seconds = datetime.now(timezone.utc).second * self.__speed_adjustment__
+        self.__incremental_index__ += elapsed_seconds
         pixel_count = configuration.CONFIG[configuration.PIXEL_COUNT_KEY]
         brightness_adjustment = configuration.get_brightness_proportion()
+
+        if self.__incremental_index__ > 1.0:
+            self.__incremental_index__ -= 1.0
+            new_color_order = [self.__colors__[-1]]
+            new_color_order.extend(self.__colors__[:-1])
+
         brightness_adjusted_colors: list = [colors_lib.get_brightness_adjusted_color(color, brightness_adjustment) for color in self.__colors__]
         color_count: int = len(brightness_adjusted_colors)
-
-        mod_second: int = int(current_seconds % color_count)
-        color_index: int = 0
+        color_index:int = 0
 
         for i in range(pixel_count):
             try:
-                relative_led_index: int = i % color_count
-                color_index = (relative_led_index + mod_second)
-                color_index = color_index if color_index < color_count else color_index - color_count
-                color_index = color_index if color_index >= 0 else color_index + color_count
+                color_index = i % color_count
                 color = brightness_adjusted_colors[color_index]
 
                 self.__renderer__.set_led(i, color)
             except Exception as ex:
-                print(f'While attempting to set LED:{i} in mod_second:{mod_second} to color_index:{color_index}. color_count:{color_count} EX={ex}')
+                print(f'While attempting to set LED:{i} with self.__incremental_index__:{self.__incremental_index__} to color_index:{color_index}. color_count:{color_count} EX={ex}')
 
         self.__renderer__.show()

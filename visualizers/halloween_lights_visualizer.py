f48793d327235b13d0f5ad6ddd5a4c74909911ba
John Marzulli <john.marzulli@hotmail.com>
2025-11-01 13:15:17 -0700

    More adjustments

--- visualizers/halloween_lights_visualizer.py
+++ visualizers/halloween_lights_visualizer.py
@@ -1,10 +1,7 @@
-from datetime import datetime, timezone
-
 from configuration import configuration
 from lib import colors as colors_lib
 from renderers.debug import Renderer
 from visualizers.visualizer import Visualizer
-
 
 class HalloweenLightsVisualizer(Visualizer):
     def __init__(self, renderer: Renderer, stations: dict):
@@ -12,7 +9,7 @@ class HalloweenLightsVisualizer(Visualizer):
         orange = [240, 173, 31]
         green = [0, 255, 0]
         purple = [160, 32, 240]
-        self.__colors__ = [off, off, orange, green, purple, off, off]
+        self.__colors__ = [off, off, off, orange, green, purple, off, off, off]
         # The lower the adjustment, the slower the lights. 0.5 is half the speed.
         # The higher the adjustment, the faster the lights. 2 is twice the speed.
         self.__speed_adjustment__ = 2
@@ -25,14 +22,10 @@ class HalloweenLightsVisualizer(Visualizer):
         pixel_count = configuration.CONFIG[configuration.PIXEL_COUNT_KEY]
         brightness_adjustment = configuration.get_brightness_proportion()
 
-        if self.__incremental_index__ > 1.0:
-            self.__incremental_index__ -= 1.0
-            new_color_order = [self.__colors__[-1]]
-            new_color_order.extend(self.__colors__[:-1])
-            self.__colors__ = new_color_order
+        self.__rotate_colors__()
 
         brightness_adjusted_colors: list = [colors_lib.get_brightness_adjusted_color(color, brightness_adjustment) for color in self.__colors__]
-        color_count: int = len(brightness_adjusted_colors)
+        color_count:int = len(brightness_adjusted_colors)
         color_index:int = 0
 
         for i in range(pixel_count):
@@ -45,3 +38,10 @@ class HalloweenLightsVisualizer(Visualizer):
                 print(f'While attempting to set LED:{i} with self.__incremental_index__:{self.__incremental_index__} to color_index:{color_index}. color_count:{color_count} EX={ex}')
 
         self.__renderer__.show()
+
+    def __rotate_colors__(self):
+        if self.__incremental_index__ > 1.0:
+            self.__incremental_index__ -= 1.0
+            new_color_order = [self.__colors__[-1]]
+            new_color_order.extend(self.__colors__[:-1])
+            self.__colors__ = new_color_order

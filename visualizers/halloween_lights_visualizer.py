9c9846a531c16b8dd62cc05e0a4c07179fd87a5d
John Marzulli <john.marzulli@hotmail.com>
2025-11-01 13:19:19 -0700

    Try slower

--- visualizers/halloween_lights_visualizer.py
+++ visualizers/halloween_lights_visualizer.py
@@ -12,7 +12,7 @@ class HalloweenLightsVisualizer(Visualizer):
         self.__colors__ = [off, off, off, orange, green, purple, off, off, off]
         # The lower the adjustment, the slower the lights. 0.5 is half the speed.
         # The higher the adjustment, the faster the lights. 2 is twice the speed.
-        self.__speed_adjustment__ = 30 # 2 seems OK
+        self.__speed_adjustment__ = 1 # 2 seems OK
         self.__incremental_index__: float = 0.0
         super().__init__(renderer, stations)
 

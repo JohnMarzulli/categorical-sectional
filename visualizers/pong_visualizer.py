59c03042c19a19a2842cc9fbd85d11d20964aa9e
John Marzulli <john.marzulli@hotmail.com>
2025-11-01 13:42:03 -0700

    Revert back to a simplified version of the set ball and trail colors.

--- visualizers/pong_visualizer.py
+++ visualizers/pong_visualizer.py
@@ -8,6 +8,8 @@ from visualizers.rainbow_visualizer import wheel
 
 class PongVisualizer(Visualizer):
     def __init__(self, renderer: Renderer, stations: dict):
+        self.__ball_color__ = [255, 255, 255]
+        self.__trail_color__ = [240, 173, 31] # Orange
         self.__off__ = [0, 0, 0]
         # The lower the adjustment, the slower the lights. 0.5 is half the speed.
         # The higher the adjustment, the faster the lights. 2 is twice the speed.
@@ -35,16 +37,11 @@ class PongVisualizer(Visualizer):
 
         self.__renderer__.set_all(self.__off__)
 
-        color = wheel(index & 255)
-        brightness_adjusted_color = colors_lib.get_brightness_adjusted_color(color, brightness_adjustment)
+        brightness_adjusted_color = colors_lib.get_brightness_adjusted_color(self.__ball_color__, brightness_adjustment)
         self.__renderer__.set_led(index, brightness_adjusted_color)
 
-        trail_brightness = brightness_adjustment
-
-        for trail_index in range(1, 3):
-            true_trail_index = index - (trail_index * self.__direction__)
-            color = wheel(true_trail_index & 255)
-            trail_brightness /= 2.0
-            self.__renderer__.set_led(true_trail_index, colors_lib.get_brightness_adjusted_color(color, trail_brightness))
+        self.__renderer__.set_led(index - self.__direction__, colors_lib.get_brightness_adjusted_color(self.__trail_color__, brightness_adjustment / 2))
+        self.__renderer__.set_led(index - (2 * self.__direction__), colors_lib.get_brightness_adjusted_color(self.__trail_color__, brightness_adjustment / 4))
+        self.__renderer__.set_led(index - (3 * self.__direction__), colors_lib.get_brightness_adjusted_color(self.__trail_color__, brightness_adjustment / 8))
 
         self.__renderer__.show()

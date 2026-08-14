664fdada1e8b996de45f1f89e230e82adb4c8e3b
John Marzulli <John.Marzulli@hotmail.com>
2026-08-14 11:11:21 -0700

    Fix issue blocking development on newer versions of Python

--- lib/local_debug.py
+++ lib/local_debug.py
@@ -22,11 +22,18 @@ def validate_python_version():
         Exception -- If the  version of Python is not new enough.
     """
 
-    python_version = float('{}.{}'.format(
-        version_info.major, version_info.minor))
-    error_text = 'Requires Python {}'.format(REQUIRED_PYTHON_VERSION)
+    major_required_python_version = int(REQUIRED_PYTHON_VERSION)
+    minor_required_python_version = int(
+        (REQUIRED_PYTHON_VERSION - major_required_python_version) * 10)
 
-    if python_version < REQUIRED_PYTHON_VERSION:
+    major_version_acceptable: bool = version_info.major >= major_required_python_version
+    minor_version_acceptable: bool = version_info.minor >= minor_required_python_version
+    python_version_acceptable: bool = major_version_acceptable and minor_version_acceptable
+
+    error_text = 'Requires Python {}, found {}.{}'.format(
+        REQUIRED_PYTHON_VERSION, version_info.major, version_info.minor)
+
+    if not python_version_acceptable:
         print(error_text)
         raise Exception(error_text)
 
